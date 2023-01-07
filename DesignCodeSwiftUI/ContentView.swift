09688d7125878f5d0045791f6e010bca7c190faf
Artur <arturremizov@gmail.com>
2023-01-07 17:55:03 +0300

    Initial Commit

---
+++ DesignCodeSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DesignCodeSwiftUI
+//
+//  Created by Artur Remizov on 7.01.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCodeSwiftUI/DesignCodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeSwiftUIApp.swift
+//  DesignCodeSwiftUI
+//
+//  Created by Artur Remizov on 7.01.23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
