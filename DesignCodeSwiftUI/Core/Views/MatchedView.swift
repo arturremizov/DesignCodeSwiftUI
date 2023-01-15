9499fce698b2abf883ac75c2fab47bd719b050ab
Artur <arturremizov@gmail.com>
2023-01-15 23:26:53 +0300

    transition between views

--- DesignCodeSwiftUI/Core/Views/MatchedView.swift
+++ DesignCodeSwiftUI/Core/Views/MatchedView.swift
@@ -15,56 +15,121 @@ struct MatchedView: View {
     var body: some View {
         ZStack {
             if !show {
-                VStack(alignment: .leading, spacing: 20.0) {
-                    Text("Hello, World!")
-                        .font(.largeTitle)
-                        .bold()
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("Second Text")
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
-                    Text("Third Description Text")
-                        .font(.footnote)
-                        .matchedGeometryEffect(id: "description", in: namespace)
-
+                VStack {
+                    Spacer()
+                    VStack(alignment: .leading, spacing: 20.0) {
+                        Text("Hello, World!")
+                            .font(.largeTitle)
+                            .bold()
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("Second Text")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+                        Text("Third Description Text")
+                            .font(.footnote)
+                            .matchedGeometryEffect(id: "description", in: namespace)
+                    }
+                    .padding(20)
+                    .background(
+                        Rectangle()
+                            .fill(.ultraThinMaterial)
+                            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                            .blur(radius: 30)
+                            .matchedGeometryEffect(id: "backgroundBlur", in: namespace)
+                    )
                 }
-                .padding(20)
                 .foregroundStyle(.white)
                 .background(
-                    Color.red
+                    Image("Illustration 9")
+                        .resizable()
+                        .scaledToFit()
+                        .matchedGeometryEffect(id: "image", in: namespace)
+                )
+                .background(
+                    Image("Background 5")
+                        .resizable()
+                        .scaledToFill()
                         .matchedGeometryEffect(id: "background", in: namespace)
                 )
+                .mask {
+                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .matchedGeometryEffect(id: "mask", in: namespace)
+                }
+                .frame(height: 300)
                 .padding(20)
 
             } else {
-                VStack(alignment: .leading, spacing: 20.0) {
-                    Spacer()
-                    Text("Third Description Text")
-                        .font(.footnote)
-                        .matchedGeometryEffect(id: "description", in: namespace)
-
-                    Text("Second Text")
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
-                    Text("Hello, World!")
-                        .font(.largeTitle)
-                        .bold()
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
+                ScrollView {
+                    VStack {
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 500)
+                    .foregroundStyle(.black)
+                    .background(
+                        Image("Illustration 9")
+                            .resizable()
+                            .scaledToFit()
+                            .matchedGeometryEffect(id: "image", in: namespace)
+                    )
+                    .background(
+                        Image("Background 5")
+                            .resizable()
+                            .scaledToFill()
+                            .matchedGeometryEffect(id: "background", in: namespace)
+                    )
+                    .mask {
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .matchedGeometryEffect(id: "mask", in: namespace)
+                    }
+                    .overlay {
+                        VStack(alignment: .leading, spacing: 20.0) {
+                            Text("Hello, World!")
+                                .font(.largeTitle)
+                                .bold()
+                                .matchedGeometryEffect(id: "title", in: namespace)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            
+                            Text("Second Text")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                                .matchedGeometryEffect(id: "subtitle", in: namespace)
+                            
+                            Text("Third Description Text")
+                                .font(.footnote)
+                                .matchedGeometryEffect(id: "description", in: namespace)
+                          
+                            Divider()
+                            
+                            HStack {
+                                Image("Avatar Default")
+                                    .resizable()
+                                    .frame(width: 26, height: 26)
+                                    .cornerRadius(10)
+                                    .padding(8)
+                                    .background(
+                                        .ultraThinMaterial,
+                                        in: RoundedRectangle(cornerRadius: 18, style: .continuous)
+                                    )
+                                    .strokeStyle(cornerRadius: 18)
+                                Text("Taught by Meng To")
+                            }
+                        }
+                        .padding(20)
+                        .background(
+                            Rectangle()
+                                .fill(.ultraThinMaterial)
+                                .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                                .matchedGeometryEffect(id: "backgroundBlur", in: namespace)
+                        )
+                        .offset(y: 250)
+                        .padding(20)
+                    }
                 }
-                .padding(20)
-                .foregroundStyle(.black)
-                .background(
-                    Color.blue
-                        .matchedGeometryEffect(id: "background", in: namespace)
-                )
             }
         }
-        
         .onTapGesture {
             withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
                 show.toggle()
